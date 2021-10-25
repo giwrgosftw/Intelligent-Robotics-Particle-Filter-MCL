23c8a14959de2689fc0c1ac0cbd8018cb7c86b4a
George Karanasios <kar.giorgos7@hotmail.com>
2021-10-25 03:56:01 -0700

    Update run.sh

--- Script/run.sh
+++ Script/run.sh
@@ -6,4 +6,8 @@ gnome-terminal -x bash -c "rosrun map_server map_server '/home/'$USER'/catkin_ws
 sleep 4
 gnome-terminal -x bash -c "rosrun stage_ros stageros '/home/'$USER'/catkin_ws/src/socspioneer/data/meeting.world'" xdotool getactivewindow windowminimize
 sleep 4
+gnome-terminal -x bash -c "rosrun tf static_transform_publisher 0 0 0 0 0 0 1 /map /odom 100" xdotool getactivewindow windowminimize
+sleep 4
+gnome-terminal -x bash -c "rosrun rviz rviz -d /home/'$USER'/catkin_ws/src/assignment_1/run.rviz" xdotool getactivewindow windowminimize
+sleep 4
 gnome-terminal -x bash -c "roslaunch socspioneer keyboard_teleop.launch" xdotool getactivewindow windowminimize
